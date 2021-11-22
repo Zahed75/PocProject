3d52664179a9296a0a4561e281ffdc4029cd1ea8
Zahed75 <Zahedhasan.CS@gmail.com>
2021-11-22 13:04:54 +0600

    admin created

--- Login_App/admin.py
+++ Login_App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+@admin.register(StudentModel)
+class StudentModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'full_name', 'batch', 'institution', 'board')
