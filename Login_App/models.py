d310d20f42e84142a2da8c128820e75f7b8fbe18
alex-pythonista <indrajit.alex@gmail.com>
2021-11-24 23:06:36 +0600

    StudentModel updated

--- Login_App/models.py
+++ Login_App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from Exam_Dashboard.models import Batch
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class StudentModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_auth')
     phone_number = models.CharField(max_length=14, unique=True, verbose_name='Phone number')
     full_name = models.CharField(max_length=100, blank=False, verbose_name='Full Name')
-    batch = models.CharField(max_length=100, blank=False, null=False)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, related_name='student_batch')
     institution = models.CharField(max_length=500, blank=False, null=False)
     board = models.CharField(max_length=100, blank=False, null=False)
     # otp 
