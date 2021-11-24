2db04f180ccf21e2a3e546006d87f84fd9bc57c0
alex-pythonista <indrajit.alex@gmail.com>
2021-11-24 22:03:41 +0600

    exam model created

--- Exam_Dashboard/models.py
+++ Exam_Dashboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from Login_App.models import *
+
 # Create your models here.
+
+class ExamModel(models.Model):
+    name = models.CharField(max_length=1000)
+    details = models.TextField()
+    # time = models.TimeField()
+    # date = models.DateField()
+    cover_photo = models.ImageField(upload_to='exam_cover_photos', blank=True, null=True)
+    
+    # assign student
+    # batch = models.ForeignKey()
+    # exam_pack = models.ForeignKey()
+
+    # marking
+    total_mark = models.IntegerField(default=0)
+    pass_mark = models.IntegerField(default=0)
+
+    isRandomized = models.BooleanField(default=False, verbose_name='Randomization')
+    isSorted = models.BooleanField(default=False, verbose_name='Sorting')
+    
+    # for negative marking
+    isNegativeMarking = models.BooleanField(default=False, verbose_name='Negative Marking')
+    amount_per_mistake = models.IntegerField(default=0, verbose_name='Amount per mistake')
+
+    def __str__(self):
+        return self.name
